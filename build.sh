eda9444562d4f3fc5370342ad6eb00671de67e04
sunhaolin <sunhaolin@hotoa.com>
2017-10-23 18:07:35 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+BUNDLE_PATH="/srv/chat"
+
+# Create BUNDLE_PATH directory if it does not exist
+[ ! -d $BUNDLE_PATH ] && mkdir -p $BUNDLE_PATH || :
+
+if [ -d "$BUNDLE_PATH" ]; then
+	echo "=> Updating..."; echo;
+	git clean -df
+	git checkout -- .
+	git checkout steedos
+	git pull
+
+	echo "=> Npm install..."; echo;
+	npm config set registry https://registry.npm.taobao.org
+	npm install -d
+	cd packages/rocketchat-katex
+	npm install -d
+	cd ../../
+
+	echo "=> Building bundle..."; echo;
+	meteor build --server https://cn.steedos.com/chat --directory $BUNDLE_PATH --allow-superuser
+	cd $BUNDLE_PATH/bundle/programs/server
+	rm -rf node_modules
+	rm -f npm-shrinkwrap.json
+	npm install --registry https://registry.npm.taobao.org -d
+
+	echo "=> Strat server..."; echo;
+	cd $BUNDLE_PATH
+	pm2 restart chat.0
+
+else
+	echo "!!!=> Failed to create bundle path: $BUNDLE_PATH"
+fi
